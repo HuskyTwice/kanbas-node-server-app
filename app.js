d9c5013ff2ef689af847e8c7f3a8ceff371c5b42
Seungju Choi <seungjuchoi@Seungjus-MBP-2.lan>
2023-11-20 18:21:17 -0800

    first commit for the server

---
+++ app.js
@@ -0,0 +1,21 @@
+// const express = require('express')
+import "dotenv/config";
+import express from 'express';
+import Hello from './hello.js';
+import Lab5 from './Lab5.js';
+import cors from "cors";
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+Hello(app);
+Lab5(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+
+app.listen(process.env.PORT || 4000); // Listen to http://localhost:4000
