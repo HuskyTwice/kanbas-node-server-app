dee19abad1f74728f487a0a47abc265cfc67ff6c
Seungju Choi <seungjuchoi@Seungjus-MBP-2.lan>
2023-11-20 19:24:43 -0800

    update

--- Database/index.js
+++ Database/index.js
@@ -4,22 +4,22 @@ import modules from "./modules.json";
 import assignments from "./assignments.json";
 import users from "./users.json";
 import grades from "./grades.json";
-import enrollments from "./enrollment.json";
+import enrollments from "./enrollments.json";
 
 // Validate the type of the imported JSON objects
-assert(typeof coursesData === "object", "courses.json is not of type object");
-assert(typeof modulesData === "object", "modules.json is not of type object");
-assert(typeof assignmentsData === "object", "assignments.json is not of type object");
-assert(typeof usersData === "object", "users.json is not of type object");
-assert(typeof gradesData === "object", "grades.json is not of type object");
-assert(typeof enrollmentsData === "object", "enrollment.json is not of type object");
+// assert(typeof courses === "object", "courses.json is not of type object");
+// assert(typeof modulesData === "object", "modules.json is not of type object");
+// assert(typeof assignmentsData === "object", "assignments.json is not of type object");
+// assert(typeof usersData === "object", "users.json is not of type object");
+// assert(typeof gradesData === "object", "grades.json is not of type object");
+// assert(typeof enrollmentsData === "object", "enrollment.json is not of type object");
 
 
 export default {
-    courses: coursesData,
-    modules: modulesData,
-    assignments: assignmentsData,
-    users: usersData,
-    grades: gradesData,
-    enrollments: enrollmentsData
+    courses,
+    modules,
+    assignments,
+    users,
+    grades,
+    enrollments
 };
